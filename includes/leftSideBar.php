23d422f066f5ba85f8bb35a63c26ff1906ccb66a
Patrik <ppimentel@andescode.cl>
2020-05-14 04:49:55 -0400

    :bug:  Maquetacion y Diseño de UI
    Plantillas de Ejemplo para reutilización.

---
+++ includes/leftSideBar.php
@@ -0,0 +1,72 @@
+<aside class="left-sidebar">
+            <!-- Sidebar scroll-->
+            <div class="scroll-sidebar">
+                <!-- Sidebar navigation-->
+                <nav class="sidebar-nav">
+                    <ul id="sidebarnav">
+                        <li class="user-profile">
+                            <a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false"><i class="mdi mdi-account"></i><span class="hide-menu">Usuario Prueba</span></a>
+                            <ul aria-expanded="false" class="collapse">
+                                <li><a href="#">Mis Datos </a></li>
+                                <li><a href="#">Mis Accesos</a></li>
+                                <li><a href="#">Mensajes</a></li>
+                                <li><a href="#">Configuracion</a></li>
+                                <li><a href="#">Logout</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-devider"></li>
+                        <li class="nav-small-cap">Administración</li>
+
+                        <li> <a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false"><i class="mdi mdi-cart"></i><span class="hide-menu">Ventas</span></a>
+                            <ul aria-expanded="false" class="collapse">
+                                <li><a href="#">Nueva Venta</a></li>
+                                <li><a href="#">Historial</a></li>
+                                <li><a href="#">Resúmen</a></li>
+                            </ul>
+                        </li>
+                        <li> <a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false"><i class="mdi mdi-barcode-scan"></i><span class="hide-menu">Productos</span></a>
+                            <ul aria-expanded="false" class="collapse">
+                                <li><a href="#">Listado </a></li>
+                                <li><a href="#">Agregar</a></li>
+                                <li><a href="#">Modificar</a></li>
+                                <li><a href="#">Stock</a></li>
+                            </ul>
+                        </li>
+                        <li> <a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false"><i class="mdi mdi-folder-account"></i><span class="hide-menu">Clientes</span></a>
+                            <ul aria-expanded="false" class="collapse">
+                                <li><a href="#">Listado </a></li>
+                                <li><a href="#">Agregar</a></li>
+                                <li><a href="#">Modificar</a></li>
+                                <li><a href="#">Agregar Favoritos</a></li>
+                            </ul>
+                        </li>
+                        <li> <a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false"><i class="mdi mdi-account-multiple"></i><span class="hide-menu">Usuarios</span></a>
+                            <ul aria-expanded="false" class="collapse">
+                                <li><a href="#">Listado </a></li>
+                                <li><a href="index2.html">Agregar</a></li>
+                                <li><a href="index3.html">Modificar</a></li>
+                            </ul>
+                        </li>
+                        <li> <a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false"><i class="mdi mdi-truck-delivery"></i><span class="hide-menu">Proveedores</span></a>
+                            <ul aria-expanded="false" class="collapse">
+                                <li><a href="#">Ingresar Orden de Compra</a></li>
+                                <li><a href="#">Listado </a></li>
+                                <li><a href="#">Agregar</a></li>
+                                <li><a href="#">Modificar</a></li>
+                                
+                            </ul>
+                        </li>
+                        <li> <a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false"><i class="mdi mdi-file-chart"></i><span class="hide-menu">Reportería</span></a>
+                            <ul aria-expanded="false" class="collapse">
+                                <li><a href="#">Ventas</a></li>
+                                <li><a href="#">Proveedores</a></li>
+                                <li><a href="#">Clientes</a></li>
+                                <li><a href="#">Compras</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </nav>
+                <!-- End Sidebar navigation -->
+            </div>
+            <!-- End Sidebar scroll-->
+        </aside>
